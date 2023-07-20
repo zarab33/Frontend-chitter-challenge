da01e020298cdf982e6f361ef56221d1277c9c47
Zara <zarabucknor@yahoo.co.uk>
2023-07-20 15:11:58 +0100

    delete files

--- chitter_api_backend-master/db/migrate/20180622173432_add_session_key_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSessionKeyToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :session_key, :string
-  end
-end
--- chitter_api_backend-master/spec/support/authorization_helpers.rb
+++
@@ -1,9 +0,0 @@
-module AuthorizationHelpers
-  def authorize!
-    request.headers["Authorization"] =  "Token token=#{user.generate_session_key!}"
-  end
-
-  def authorize_badly!
-    request.headers["Authorization"] =  "Token token=horse"
-  end
-end
